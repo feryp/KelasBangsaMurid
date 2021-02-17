9dc6a3943fcd89ab70a9d011d9eacc5f6c4f6d52
Fery Pratama <37443445+feryp@users.noreply.github.com>
2021-02-17 10:03:14 +0700

    add splash screen , and edit environment

---
+++ app/src/main/java/com/kelasbangsa/murid/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.kelasbangsa.murid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+    }
+}
