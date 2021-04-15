f8da19ad3f6dde5f5c552d1a22ea0a98321a771a
Fery Pratama <37443445+feryp@users.noreply.github.com>
2021-04-15 09:10:35 +0700

    add View.OnClickListener di SignInActivity.kt

--- app/src/main/java/com/kelasbangsa/murid/SignInActivity.kt
+++ app/src/main/java/com/kelasbangsa/murid/SignInActivity.kt
@@ -3,13 +3,14 @@ package com.kelasbangsa.murid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.button.MaterialButton
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), View.OnClickListener {
 
-    var btnSignIn : MaterialButton? = null
-    var btnToSignUp : MaterialButton? = null
-    var btnForgotPassword : MaterialButton? = null
+    private lateinit var btnSignIn : MaterialButton
+    private lateinit var btnToSignUp : MaterialButton
+    private lateinit var btnForgotPassword : MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,19 +22,27 @@ class SignInActivity : AppCompatActivity() {
         btnForgotPassword = findViewById(R.id.btn_forgot_password)
 
         //Set OnClick Listener
-        btnSignIn?.setOnClickListener{
-            val signIn = Intent(this, MainActivity::class.java)
-            startActivity(signIn)
-        }
+        btnSignIn.setOnClickListener(this)
+        btnToSignUp.setOnClickListener(this)
+        btnForgotPassword.setOnClickListener(this)
+    }
 
-        btnToSignUp?.setOnClickListener{
-            val toSignUp = Intent(this,SignUpActivity::class.java)
-            startActivity(toSignUp)
-        }
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btn_sign_in -> {
+                val signIn = Intent(this, MainActivity::class.java)
+                startActivity(signIn)
+            }
 
-        btnForgotPassword?.setOnClickListener {
-            val forgotPassword = Intent(this, LupaSandiActivity::class.java)
-            startActivity(forgotPassword)
+            R.id.btn_to_sign_up -> {
+                val toSignUp = Intent(this,SignUpActivity::class.java)
+                startActivity(toSignUp)
+            }
+
+            R.id.btn_forgot_password -> {
+                val forgotPassword = Intent(this, LupaSandiActivity::class.java)
+                startActivity(forgotPassword)
+            }
         }
     }
 }
