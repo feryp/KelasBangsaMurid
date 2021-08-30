fbe3abf95c4f9fd1aa9cd5f40f12d23af354154a
Fery Pratama <37443445+feryp@users.noreply.github.com>
2021-08-30 16:47:38 +0700

    update ui : - add ui pin code

--- app/src/main/java/com/kelasbangsa/murid/ui/tarikSaldo/BotSheetPenarikanFragment.kt
+++ app/src/main/java/com/kelasbangsa/murid/ui/tarikSaldo/BotSheetPenarikanFragment.kt
@@ -1,5 +1,6 @@
 package com.kelasbangsa.murid.ui.tarikSaldo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
 import com.kelasbangsa.murid.R
+import com.kelasbangsa.murid.ui.pin.PinCodeActivity
+import com.kelasbangsa.murid.ui.sandiBaru.GantiKataSandiActivity
 
 class BotSheetPenarikanFragment : BottomSheetDialogFragment() {
 
     private lateinit var btnClose : ImageButton
+    private lateinit var btnKirimSaldo : MaterialButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +27,12 @@ class BotSheetPenarikanFragment : BottomSheetDialogFragment() {
 
         //INIT VIEW
         btnClose = view.findViewById(R.id.btn_close)
+        btnKirimSaldo = view.findViewById(R.id.btn_kirim_penarikan)
+
+        btnKirimSaldo.setOnClickListener {
+            val kirimSaldo = Intent(context, PinCodeActivity::class.java)
+            startActivity(kirimSaldo)
+        }
 
         return view
     }
